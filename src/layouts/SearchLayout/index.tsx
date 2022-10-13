0bffd6a73cf31058fa4b3fff217390a6226c7749
Dmitrii Arbatskii <dmitrii_arbatskii@epam.com>
2022-10-13 03:25:28 +0300

    refactor search logic

--- src/layouts/SearchLayout/index.tsx
+++ src/layouts/SearchLayout/index.tsx
@@ -2,27 +2,32 @@ import React, {useState} from "react";
 
 import {searchIsClose} from "../../redux/isSearchOpen.slice";
 import {useLazySearchByCityQuery} from "../../redux/weather.api";
-import {chooseCity} from "../../redux/currentCity.slice";
-import {useAppDispatch} from "../../redux";
+import {addCityToHistory, chooseCity, clearHistory} from "../../redux/currentCity.slice";
+import {useAppDispatch, useAppSelector} from "../../redux";
 import {ICity} from "../../interfaces";
 
 import css from "./SearchLayout.module.scss";
 
 export const SearchLayout = () => {
   const [search, setSearch] = useState('')
+  const cityHistory = useAppSelector(state => state.currentCity.search_history)
   const dispatch = useAppDispatch()
   const [fetchCities, {isLoading: citiesLoading, data: cities}] = useLazySearchByCityQuery()
 
   const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
     setSearch(e.currentTarget.value)
-    fetchCities(search)
+    if (search.length > 2) {
+      fetchCities(search)
+    }
   }
 
-  const handleButtonClick = () => {
-    fetchCities(search)
-    dispatch(chooseCity(search))
+  const handleButtonClick = (input: string) => {
+    setSearch(input)
+    fetchCities(input)
+    dispatch(chooseCity(input))
     dispatch(searchIsClose())
     setSearch('')
+    dispatch(addCityToHistory(input))
   }
 
   return (
@@ -37,20 +42,25 @@ export const SearchLayout = () => {
           value={search}
           onChange={e => handleInputChange(e)}
         />
-        <button
-          className={css.searchSubmit}
-          onClick={handleButtonClick}
-        >Search</button>
       </div>
       <div>
         <ul>
           {citiesLoading && <p>Loading...</p>}
           {cities && cities.map((city: ICity) => (
-            <li className={css.city}>
+            <li className={css.city} key={city.id}>
               <button
                 className={css.cityButton}
-                onClick={handleButtonClick}
+                onClick={() => handleButtonClick(city.name)}
               >{city.name}</button>
+            </li>
+          ))}
+          {cityHistory.length > 0 && <button onClick={() => dispatch(clearHistory())}>Clean</button>}
+          {cityHistory && cityHistory.map((city) => (
+            <li className={css.city} key={city}>
+              <button
+                className={css.cityButton}
+                onClick={() => handleButtonClick(city)}
+              >{city}</button>
             </li>
           ))}
         </ul>
--- src/redux/currentCity.slice.ts
+++ src/redux/currentCity.slice.ts
@@ -2,10 +2,12 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 type currentCityState = {
   value: string,
+  search_history: string[]
 }
 
 const initialState: currentCityState = {
-  value: 'moscow'
+  value: 'moscow',
+  search_history: []
 }
 
 export const currentCitySlice = createSlice({
@@ -14,11 +16,19 @@ export const currentCitySlice = createSlice({
   reducers: {
     chooseCity: (state, action: PayloadAction<string>) => {
       state.value = action.payload
-    }
+    },
+    addCityToHistory: (state, action: PayloadAction<string>) => {
+      state.search_history.push(action.payload)
+      state.search_history = [...new Set(state.search_history)]
+      state.search_history = state.search_history.filter(city => city.length !== 0)
+    },
+    clearHistory: (state) => {
+      state.search_history = []
+    },
   }
 })
 
-export const { chooseCity } = currentCitySlice.actions
+export const { chooseCity, addCityToHistory, clearHistory } = currentCitySlice.actions
 
 export const currentCityReducer =  currentCitySlice.reducer
 
